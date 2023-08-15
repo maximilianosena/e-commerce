59b7cf3237b25e9ef01b8e8474b3aab91b044be4
maximilianosena <maximilianosena13@gmail.com>
2023-08-15 11:42:23 -0300

    Desafiate
    index.html en nav, init.js y login.js

--- js/init.js
+++ js/init.js
@@ -39,3 +39,36 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+//////////////////////////////////////////////
+
+const profile= document.getElementById("login")
+
+function sesion_existe(){  
+
+  //utilizo misma lógica que en login.js
+  const savedSession = localStorage.getItem("usuarios"); 
+   //Obtengo los elementos del localStorage y si existen, parsea los elementos obtenidos en la constante savedSession
+  if (savedSession)
+  {
+    usuarios_Locales = JSON.parse(savedSession);
+
+      console.log("Sesiones existentes en localStorage:", usuarios_Locales);
+      console.log("Cantidad de usuarios guardados: " + usuarios_Locales.length)
+      
+      //Busco la última sesión iniciada, obtengo la ubicación, el indice
+      let lastSession= usuarios_Locales.length - 1;
+      
+      //Corroboro la información en consola, numero de indice en el array
+      console.log("Numero de indice de la última sesión: " + lastSession)
+      
+      //Busco en el array "usuarios_Locales" el valor de lastSession, la propiedad Nombre, para que aparezca como texto
+      profile.innerText = 'Bienvenido: ' + usuarios_Locales[lastSession].Nombre
+  } else {
+
+    //Si no existe elementos en localStorage, redirecciona al login.html
+    location.replace("login.html")
+  }
+  }
+
+  sesion_existe();
